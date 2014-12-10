8f80f77043bdf40f9c891063da1468b924d67cb2
matmenzl <mathias.menzl@gmail.com>
2014-12-10 13:15:01 +0100

    Initial Commit

---
+++ unic-beacon-demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  unic-beacon-demo
+//
+//  Created by Mathias Menzl on 10/12/14.
+//  Copyright (c) 2014 Mathias Menzl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
